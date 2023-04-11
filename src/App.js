0d92bc1431bb82b7f8ae59e51545c33226e108db
noahwhlim <noahhlimwh@gmail.com>
2023-04-11 12:04:56 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import items from "./products_json.json"
+import {Products} from "./Products.js"
+
+
+const App = () => {
+  const [ProductsCategory, setProductsCategory] = useState(Products);
+  const [query, setQuery] = useState('');
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    total();
+  }, [cart]);
+
+  const total = () => {
+    let totalVal = 0;
+    for (let i = 0; i < cart.length; i++) {
+        totalVal += cart[i].price;
+    }
+    setCartTotal(totalVal);
+  };  
+
+  const addToCart = (el) => {
+    setCart([...cart, el]);
+    console.log("increase")
+  };
+
+  const removeFromCart = (el) => {
+    let hardCopy = [...cart];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
+    setCart(hardCopy);
+  };
+
+  function howManyofThis(id) {
+    let hmot = cart.filter((cartItem) => cartItem.id === id);
+    return hmot.length;
+  } 
+
+  const cartItems = cart.map((el) => (
+    <div key={el.id}>
+        <img class="img-fluid" src={el.image} width={30} />
+        {el.title}
+        ${el.price}
+    </div>
+  )); 
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    // console.log("Step 6 : in handleChange, Target Value :",e.target.value,"  Query Value :",query);
+    const results = Products.filter(eachProduct =>{
+    if (e.target.value === "") return ProductsCategory;
+      return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase())
+    });
+    setProductsCategory(results);
+  }
+
+  const render_products = (ProductsCategory) => {
+    return (
+      <div className='category-section fixed'>
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
+        <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: '800px'}}>
+          {ProductsCategory.map((el, index) => (
+            <div className="row mb-4 d-flex justify-content-between align-items-center">
+            <div className="col-md-2 col-lg-2 col-xl-2">
+              <img
+                src={el.image}
+                className="img-fluid rounded-3" alt={el.description} />
+            </div>
+            <div className="col-md-3 col-lg-3 col-xl-3">
+              <h6 className="text-muted">{el.title}</h6>
+              <h6 className="text-black mb-0">{el.title}</h6>
+            </div>
+            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+              <button
+                type="button"
+                variant="light"
+                onClick={() => removeFromCart(el)}
+              >
+                {" "}
+                -{" "}
+              </button>{" "}
+              <button type="button" variant="light" onClick={() => addToCart(el)}>
+                {" "}
+                +{" "}
+              </button>
+            </div>
+      
+            <div class="col">
+              ${el.price} <span class="close">&#10005;</span>
+              {howManyofThis(el.id)}
+            </div>
+            <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+              <a href="#!" className="text-muted"><i className="fas fa-times"></i></a>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  const listItems = items.map(el => (
+    <div className="row mb-4 d-flex justify-content-between align-items-center">
+      <div className="col-md-2 col-lg-2 col-xl-2">
+        <img
+          src={el.image}
+          className="img-fluid rounded-3" alt={el.description} />
+      </div>
+      <div className="col-md-3 col-lg-3 col-xl-3">
+        <h6 className="text-muted">{el.title}</h6>
+        <h6 className="text-black mb-0">{el.title}</h6>
+      </div>
+      <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+        <button
+          type="button"
+          variant="light"
+          onClick={() => removeFromCart(el)}
+        >
+          {" "}
+          -{" "}
+        </button>{" "}
+        <button type="button" variant="light" onClick={() => addToCart(el)}>
+          {" "}
+          +{" "}
+        </button>
+      </div>
+
+      <div class="col">
+        ${el.price} <span class="close">&#10005;</span>
+        {howManyofThis(el.id)}
+      </div>
+      <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+        <h6 className="mb-0">${el.price}</h6>
+      </div>
+      <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+        <a href="#!" className="text-muted"><i className="fas fa-times"></i></a>
+      </div>
+    </div>
+  ));
+
+
+  return (
+    <section className="h-100 h-custom">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12">
+            <div className="card card-registration card-registration-2">
+              <div className="card-body p-0">
+                <div className="row g-0">
+                  <div className="col-lg-8">
+                    <div className="p-5">
+                      <div className="d-flex justify-content-between align-items-center mb-5">
+                        <h1 className="fw-bold mb-0 text-black">Shopping Cart</h1>
+                        <input className="mr-5" placeholder="Search" type="search" value={query} onChange={handleChange}/>
+                        <h6 className="mb-0 text-muted">{cart.length} items</h6>
+                      </div>
+                        {console.log("Before render :", Products.length, ProductsCategory.length)}
+                        {render_products(ProductsCategory)}
+                      </div>
+                  </div>
+                  <div className="col-lg-4 bg-grey">
+                    <div className="p-5">
+                      <h3 className="fw-bold mb-5 mt-2 pt-1">Summary</h3>
+
+
+                      <div className="d-flex justify-content-between mb-4">
+                        <h5 className="text-uppercase">{cart.length} items</h5>
+                        <h5>${cartTotal.toFixed(2)}</h5>
+                      </div>
+
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/Products.js
@@ -0,0 +1,44 @@
+export const Products = [
+    {
+        "id": 1,
+        "title": "Backpack",
+        "price": 54.99,
+        "description": "This is a backpack",
+        "image": "https://i5.walmartimages.com/asr/47566cee-c544-4b88-bfa8-5ec8c1f43081.f4e6d4dc45159ae5783ca0aefcc11f00.jpeg"
+    },
+    {
+        "id": 2,
+        "title": "Tshirt",
+        "price": 14.99,
+        "description": "This is a tshirt",
+        "image": "https://www.rei.com/media/product/163975"
+    },
+    {
+        "id": 3,
+        "title": "Socks",
+        "price": 4.99,
+        "description": "This is a pair of socks",
+        "image": "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/279fb0d0-b666-4cda-aec9-d53b8dd2853e/everyday-plus-cushioned-training-ankle-socks-6-pairs-xql9Hn.png"
+    },
+    {
+        "id": 4,
+        "title": "Cap",
+        "price": 14.99,
+        "description": "Cap fr",
+        "image": "https://dks.scene7.com/is/image/GolfGalaxy/16NEWMMLBYNKSNVYCAPA?qlt=70&wid=600&fmt=pjpeg"
+    },
+    {
+        "id": 5,
+        "title": "Underwear",
+        "price": 4.99,
+        "description": "This is a pair of underwear",
+        "image": "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81C5EXF+SqL._AC_UY1000_.jpg"
+    },
+    {
+        "id": 6,
+        "title": "Shorts",
+        "price": 24.99,
+        "description": "This is a pair of shorts",
+        "image": "https://static01.nyt.com/images/2021/05/28/fashion/mens-style/28sp-gear-inyt1/oakImage-1621867392181-superJumbo.jpg"
+    }
+]
